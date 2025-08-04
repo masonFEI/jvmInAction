5be3831e1446a70eb88cbbab7df9c176cfe90ac0
masonFei <994085848@qq.com>
2025-08-04 22:21:51 +0800

    14,jvm的生命周期

--- src/main/java/chapter01/StackStructTest.java
+++ src/main/java/chapter01/StackStructTest.java
@@ -8,6 +8,13 @@ public class StackStructTest {
         int i = 2;
         int j = 3;
         int k = i + j;
+        try {
+            Thread.sleep(6000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("hello");
     }
 
 }
