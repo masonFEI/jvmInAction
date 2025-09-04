34c307302646d8e614d0581cf6dc00cdbb741b51
masonFei <994085848@qq.com>
2025-09-04 09:07:28 +0800

    53-涉及操作数栈的字节码指令执行分析

---
+++ src/main/java/chapter05/OperandStackTest.java
@@ -0,0 +1,14 @@
+package chapter05;
+
+public class OperandStackTest {
+
+
+    public void testAddOperation() {
+        // byte,short,char,boolean:都以1int型来保存3
+        byte i = 15;
+        int j = 8;
+        int k = i + j;
+    }
+
+
+}
