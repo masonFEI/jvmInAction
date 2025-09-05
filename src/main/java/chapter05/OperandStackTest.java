fc6c9b6191fa9b75c1620c341ab7af73817b0bdb
masonFei <994085848@qq.com>
2025-09-05 09:07:40 +0800

    53-涉及操作数栈的字节码指令执行分析

--- src/main/java/chapter05/OperandStackTest.java
+++ src/main/java/chapter05/OperandStackTest.java
@@ -8,6 +8,22 @@ public class OperandStackTest {
         byte i = 15;
         int j = 8;
         int k = i + j;
+
+        int m = 800;
+    }
+
+
+    public int getSum() {
+        int m = 10;
+        int n = 20;
+        int k = m + n;
+        return k;
+    }
+
+    public void testGetSum() {
+        // 获取上一个栈帧返回的结果，并保存在操作数栈中
+        int i = getSum();
+        int j = 10;
     }
 
 
