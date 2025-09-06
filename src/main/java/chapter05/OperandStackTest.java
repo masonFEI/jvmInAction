57c56368e64e1792b06f7d06bba4c65c68b96995
masonFei <994085848@qq.com>
2025-09-06 23:21:05 +0800

    53-涉及操作数栈的字节码指令执行分析

--- src/main/java/chapter05/OperandStackTest.java
+++ src/main/java/chapter05/OperandStackTest.java
@@ -26,5 +26,37 @@ public class OperandStackTest {
         int j = 10;
     }
 
+    /**
+     * 程序员面试过程中，常见的i++ 和++i的区别，放到字节码篇章时再介绍。
+     */
+    public void add() {
+        // 第1类问题
+        int i1 = 10;
+        i1++;
+
+        int i2 = 10;
+        ++i2;
+
+        // 第2类问题
+        int i3 = 10;
+        int i4 = i3++;
+
+        int i5 = 10;
+        int i6 = ++i5;
+
+        // 第3类问题
+        int i7 = 10;
+        i7 = i7++;
+
+        int i8 = 10;
+        i8 = ++i8;
+
+        // 第4类问题
+        int i9 = 10;
+        int i10 = i9++ + ++i9;
+
+
+    }
+
 
 }
