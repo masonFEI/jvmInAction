981d6f7378362f8c85bc8cf42b3909cfacbe3138
masonFei <994085848@qq.com>
2025-10-27 09:08:57 +0800

    81-小结堆空间的常用参数设置

---
+++ src/main/java/chapter08/HeapArgsTest.java
@@ -0,0 +1,14 @@
+package chapter08;
+
+/**
+ * 测试堆空间常用的jvm参数：
+ * -XX:+PrintFlagsInitial: 查看所有的参数的默认初始值
+ *
+ */
+public class HeapArgsTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
