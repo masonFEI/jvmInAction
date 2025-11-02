5ea4964261a6f27fe559298ce1d19174a2ff724c
masonFei <994085848@qq.com>
2025-11-02 12:56:29 +0800

    90-设置方法区大小的参数

---
+++ src/main/java/chapter09/MethodAreaDemo.java
@@ -0,0 +1,22 @@
+package chapter09;
+
+/**
+ * 测试设置方法区大小参数的默认值
+ *
+ * jdk8及以后
+ * -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m
+ */
+public class MethodAreaDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println("start...");
+
+        try {
+            Thread.sleep(2000000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
