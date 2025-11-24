f3375c5c68dae8f2ea7d64ce7d1e662ea4ffa5e8
masonFei <994085848@qq.com>
2025-11-24 09:10:24 +0800

    119-String底层Hashtable结构的说明

---
+++ src/main/java/chapter13/StringExer.java
@@ -0,0 +1,22 @@
+package chapter13;
+
+public class StringExer {
+
+    String str = "good";
+    char[] ch = {'t', 'e', 's', 't'};
+
+
+    public void change(String str, char[] ch) {
+        str = "test ok";
+        ch[0] = 'b';
+    }
+
+
+    public static void main(String[] args) {
+        StringExer ex = new StringExer();
+        ex.change(ex.str,  ex.ch);
+        System.out.println(ex.str);//good
+        System.out.println(ex.ch);//best
+    }
+
+}
