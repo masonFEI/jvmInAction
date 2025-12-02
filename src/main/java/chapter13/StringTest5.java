8f9ae5f730d800ef402c70256a2be92eae956a1c
masonFei <994085848@qq.com>
2025-12-02 21:58:39 +0800

    122-字符串拼接操作的面试题讲解

--- src/main/java/chapter13/StringTest5.java
+++ src/main/java/chapter13/StringTest5.java
@@ -6,6 +6,25 @@ public class StringTest5 {
     public static void main(String[] args) {
         String s1 = "javaEE";
         String s2 = "hadoop";
+        String s3 = "javaEEhadoop";
+        String s4 = "javaEE" + "hadoop";
+        // 如果拼接符号的前后出现了变量，则相当于在堆空间中new String(),具体的内容为拼接的结果：javaEEhadoop
+        String s5 = s1 + "hadoop";
+        String s6 = "javaEE" + s2;
+        String s7 = s1 + s2;
+
+        System.out.println(s3 == s4);
+        System.out.println(s3 == s5);
+        System.out.println(s3 == s6);
+        System.out.println(s3 == s7);
+        System.out.println(s5 == s6);
+        System.out.println(s5 == s7);
+        System.out.println(s6 == s7);
+
+        // intern():判断字符串常量池中是否存在javaEEhadoop，如果存在，则返回常量池中javaEEhadoop的地址
+        // 如果字符串常量池中不存在javaEEhadoop，则在常量池中加载一份javaEEhadoop，并返回此对象的地址
+        String s8 = s6.intern();
+        System.out.println(s3 == s8);
     }
 
 }
