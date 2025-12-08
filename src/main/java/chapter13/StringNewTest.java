922ff84a77070791f54edff22adf8d2b3a2e15fc
masonFei <994085848@qq.com>
2025-12-08 23:12:48 +0800

    126-new String() 到底创建了几个对象

---
+++ src/main/java/chapter13/StringNewTest.java
@@ -0,0 +1,28 @@
+package chapter13;
+
+/**
+ * new String("ab") 会创建几个对象？看字节码，就知道是两个
+ * 一个对象是：new关键字在堆空间创建的
+ * 另一个对象是：字符串常量池中的对象。字节码指令：ldc
+ *
+ * new String("a") + new String("b") 呢？
+ * 对象1：new StringBuilder()
+ * 对象2：new String();
+ * 对象3：常量池中的“a”
+ * 对象4：new String();
+ * 对象5：常量池中的“b”
+ *
+ *
+ * 深入剖析：StringBuilder的toString();(看StringBuilder的字节码的toString()方法)
+ * 对象6：new String("ab")
+ * 强调一下，toString()的调用，在字符串常量池中，没有生成“ab”
+ */
+public class StringNewTest {
+
+    public static void main(String[] args) {
+//        String str = new String("ab");
+
+        String str = new String("a") + new String("b");
+    }
+
+}
