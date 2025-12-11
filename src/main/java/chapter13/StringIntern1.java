226c11c60291ee8b43754ee6bf302f1e0ecf254c
feixuanyu <994085848@qq.com>
2025-12-11 22:31:15 +0800

    优化文档

--- src/main/java/chapter13/StringIntern1.java
+++ src/main/java/chapter13/StringIntern1.java
@@ -3,7 +3,7 @@ package chapter13;
 public class StringIntern1 {
 
     public static void main(String[] args) {
-//        StringIntern.java的扩展
+        //  StringIntern.java的扩展
         String s3 = new String("1") + new String("1");
         String s4 = "11";// 在字符串常量池中生成对象“11”
         s3.intern();// 常量池中已存在“11”,相当于什么都没做
