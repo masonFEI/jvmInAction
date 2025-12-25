cd2f0983317987ab9f400e14e0487efc8ccec1fa
masonFei <994085848@qq.com>
2025-12-25 22:47:44 +0800

    146-使用JProfiler分析OOM

---
+++ src/main/java/chapter15/HeapOOM.java
@@ -0,0 +1,27 @@
+package chapter15;
+
+import java.util.ArrayList;
+
+/**
+ * -Xms8m -Xmx8m -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapOOM {
+
+    byte[] buffer = new byte[1 * 1024 * 1024];// 1MB
+
+    public static void main(String[] args) {
+        ArrayList<HeapOOM> list = new ArrayList<>();
+        int count = 0;
+        try {
+            while (true) {
+                list.add(new HeapOOM());
+                count++;
+            }
+        } catch (Exception e) {
+            System.out.println("count = " + count);
+            e.printStackTrace();
+        }
+
+    }
+
+}
