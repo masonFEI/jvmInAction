ff3dc1a3d05efb76ce168d98332a52fe42106ef9
feixuanyu <994085848@qq.com>
2026-01-01 10:46:38 +0800

    183-CMS的特点和弊端分析

--- src/main/java/chapter17/GCUseTest.java
+++ src/main/java/chapter17/GCUseTest.java
@@ -12,6 +12,15 @@ import java.util.ArrayList;
  * -XX:+UseSerialGC 参数可以指定年轻代和老年代都使用串行收集器，等价于新生代用Serial GC且老年代用Serial Old收集器
  * -XX:+UseParNewGC 参数可以指定年轻代使用并行收集器，老年代使用Serial Old收集器
  * -XX:+UseConcMarkSweepGC 参数可以指定年轻代使用并行收集器，老年代使用CMS收集器
+ * 
+ * 二者可以相互激活：
+ * -XX:+UseParallelGC 参数可以指定年轻代和老年代都使用并行收集器，等价于新生代用Parallel GC且老年代用Parallel Old收集器
+ * -XX:+UseParallelOldGC 参数可以指定年轻代使用并行收集器，老年代使用Parallel Old收集器
+ * -XX:MaxGCPauseMillis=200 设置最大暂停时间为200ms
+ * -XX:GCTimeRatio=9 设置GC时间与程序运行时间的比例=1/(N+1)
+ * 取值范围是0~99，默认值是99，表示GC时间占1%，程序运行时间占99%，不要超过99，否则会出现频繁GC，影响程序运行效率
+ * -XX:+UseAdaptiveSizePolicy 开启自适应调整堆内存大小策略
+ * 
  *
  * @author feixuanyu
  * @version 1.0.0
