7c39bdc123d618120adb9642a48097f65ae2bcff
masonFei <994085848@qq.com>
2026-01-17 19:41:35 +0800

    211-class文件本质和内部数据类型

---
+++ src/main/java/part02/chapter01/Demo.java
@@ -0,0 +1,23 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part02.chapter01;
+
+/**
+ * Demo
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-01-17 19:30
+ */
+public class Demo {
+
+    private int num = 1;
+
+    public int add() {
+        num = num + 2;
+        return num;
+    }
+
+}
