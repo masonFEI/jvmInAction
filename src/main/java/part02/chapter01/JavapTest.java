71b263d98fc0b96a9bc71fc9ccf90f1a087a96a6
masonFei <994085848@qq.com>
2026-01-31 21:30:12 +0800

    237-javap使用小结

---
+++ src/main/java/part02/chapter01/JavapTest.java
@@ -0,0 +1,53 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part02.chapter01;
+
+/**
+ * JavapTest
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-01-31 19:54
+ */
+public class JavapTest {
+
+    private int             num;
+    boolean                 flag;
+    protected char          gender;
+    public String           info;
+
+    public static final int COUNTS = 1;
+    static {
+        String url = "www.atguigu.com";
+    }
+    {
+        info = "java";
+    }
+
+    public JavapTest() {
+
+    }
+
+    private JavapTest(boolean flag) {
+        this.flag = flag;
+    }
+
+    private void methodPrivate() {
+    }
+
+    int getNum(int i) {
+        return num + i;
+    }
+
+    protected char showGender() {
+        return gender;
+    }
+
+    public void showInfo() {
+        int i = 10;
+        System.out.println(info + i);
+    }
+
+}
