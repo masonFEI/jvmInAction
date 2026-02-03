75a227ce97480ff5130acd55b926eeaaf9ee7775
masonFei <994085848@qq.com>
2026-02-03 22:36:30 +0800

    253-数组操作指令

--- src/main/java/part02/chapter02/NewTest.java
+++ src/main/java/part02/chapter02/NewTest.java
@@ -28,4 +28,37 @@ public class NewTest {
         String[][] strArray = new String[10][5];
     }
 
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+
+    public void setOrderId() {
+        Order order = new Order();
+        order.id = 1001;
+        System.out.println(order.id);
+
+        Order.name = "ORDER";
+        System.out.println(Order.name);
+    }
+
+    // 3. 数组操作指令
+    public void setArray() {
+        int[] intArray = new int[10];
+        intArray[3] = 20;
+        System.out.println(intArray[1]);
+
+        boolean[] arr = new boolean[10];
+        arr[1] = true;
+    }
+
+    public void arrLength() {
+        double[] arr = new double[10];
+        System.out.println(arr.length);
+    }
+
+}
+
+class Order {
+    int           id;
+    static String name;
 }
