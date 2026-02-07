45adc842feaaba4c66fc756a352317b3b0f7eda3
masonFei <994085848@qq.com>
2026-02-07 23:49:06 +0800

    257-操作数栈管理指令

---
+++ src/main/java/part02/chapter02/StackOperateTest.java
@@ -0,0 +1,36 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part02.chapter02;
+
+/**
+ * StackOperateTest
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-02-07 23:30
+ */
+public class StackOperateTest {
+
+    public void print() {
+        Object obj = new Object();
+        // String info = obj.toString();
+        obj.toString();
+    }
+
+    public void foo() {
+        bar();
+    }
+
+    public long bar() {
+        return 0;
+    }
+
+    public long nexIndex() {
+        return index++;
+    }
+
+    private long index = 0;
+
+}
