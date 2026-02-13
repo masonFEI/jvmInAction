4c597ac128a27c10041cbd1849eba01778ea430c
feixuanyu <994085848@qq.com>
2026-02-13 20:00:54 +0800

    264-异常处理与异常表

--- src/main/java/part02/chapter02/ExceptionTest.java
+++ src/main/java/part02/chapter02/ExceptionTest.java
@@ -4,6 +4,9 @@
  */
 package part02.chapter02;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -35,5 +38,30 @@ public class ExceptionTest {
         System.out.println(j);
     }
 
+    public void tryCatch() {
+        try {
+            File file = new File("D:\\hello.txt");
+            FileInputStream fis = new FileInputStream(file);
+            String info = "hello!";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String func() {
+        String str = "hello";
+
+        try {
+            return str;
+        } finally {
+            str = "atguigu";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(func());
+    }
 
 }
