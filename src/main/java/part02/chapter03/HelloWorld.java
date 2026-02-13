f3a21ca0c0b56fa646766c2c2ee03e9eecd0d791
feixuanyu <994085848@qq.com>
2026-02-13 21:24:29 +0800

    267-加载完成的操作及二进制的获取

---
+++ src/main/java/part02/chapter03/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part02.chapter03;
+
+/**
+ * Helloworld
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-02-13 21:14
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
