7fd8341ec32cd0456745302f4da4f02cfa721834
feixuanyu <994085848@qq.com>
2026-02-15 00:00:19 +0800

    268-类模型与Class实例的位置

--- src/main/java/part02/chapter03/LoadingTest.java
+++ src/main/java/part02/chapter03/LoadingTest.java
@@ -19,13 +19,18 @@ public class LoadingTest {
     public static void main(String[] args) {
         try {
             Class<?> clazz = Class.forName("java.lang.String");
+            // 获取当前运行时类声明的所有方法
             Method[] ms = clazz.getDeclaredMethods();
             for (Method m : ms) {
+                // 获取方法的修饰符
                 String mod = Modifier.toString(m.getModifiers());
                 System.out.println(mod + " ");
+                // 获取方法的返回值类型
                 String returnType = m.getReturnType().getSimpleName();
                 System.out.println(returnType + " ");
+                // 获取方法名称
                 System.out.println(m.getName() + "(");
+                // 获取方法的参数列表
                 Class<?>[] ps = m.getParameterTypes();
                 if (ps.length == 0) {
                     System.out.println(")");
@@ -33,12 +38,13 @@ public class LoadingTest {
 
                 for (int i = 0; i < ps.length; i++) {
                     char end = (i == ps.length - 1) ? ')' : ',';
-                    System.out.println(ps[i].getSimpleName()+end);
+                    // 获取参数的类型
+                    System.out.println(ps[i].getSimpleName() + end);
                 }
                 System.out.println();
             }
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
