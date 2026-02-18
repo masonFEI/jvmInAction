c9049094051aec2879c4bbc6e4c910a33720f04d
feixuanyu <994085848@qq.com>
2026-02-18 22:32:16 +0800

    276-类的主动使用1

---
+++ src/main/java/part02/chapter03/ActiveUse1.java
@@ -0,0 +1,71 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part02.chapter03;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * 测试类的主动使用：意味着会调用类的<clinit>(),即执行了类的初始化
+ * <p>
+ * 1.当创建一个类的实例时，比如使用new关键字，或者通过反射、克隆、反序列化
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-02-18 21:49
+ */
+public class ActiveUse1 {
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        test2();
+    }
+
+    // 序列化的过程：
+    public static void test1() {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("order.dat"));
+            oos.writeObject(new Order());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (Objects.nonNull(oos)) {
+                    oos.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 反序列化的过程：（验证）
+    public static void test2() {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(new FileInputStream("order.dat"));
+            Order order = (Order) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (Objects.nonNull(ois)) {
+                    ois.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
+
+class Order implements Serializable {
+    static {
+        System.out.println("Order类的初始化过程");
+    }
+}
