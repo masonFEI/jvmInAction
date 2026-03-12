ccc43f95adcc990ccc60c18ca48de66096fcf905
masonFei <994085848@qq.com>
2026-03-12 23:53:43 +0800

    309-jstat:基本语法

---
+++ src/main/java/part03/chapter02/ScannerTest.java
@@ -0,0 +1,23 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part03.chapter02;
+
+import java.util.Scanner;
+
+/**
+ * ScannerTest
+ *
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-03-12 23:45
+ */
+public class ScannerTest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+    }
+
+}
