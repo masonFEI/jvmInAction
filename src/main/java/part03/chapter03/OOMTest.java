d34e6a0ae962c59b74807fedb86b5f6325b0a514
masonFei <994085848@qq.com>
2026-03-20 23:14:53 +0800

    326-VisualVM的CPU抽样和内存抽样

---
+++ src/main/java/part03/chapter03/OOMTest.java
@@ -0,0 +1,40 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2026 All Rights Reserved.
+ */
+package part03.chapter03;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * OOMTest
+ *
+ * -Xms600m -Xmx600m -XX:SurvivorRatio=8
+ * @author feixuanyu
+ * @version 1.0.0
+ * @since 2026-03-20 22:44
+ */
+public class OOMTest {
+
+    public static void main(String[] args) {
+        ArrayList<Picture> list = new ArrayList<Picture>();
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            list.add(new Picture(new Random().nextInt(100 * 50)));
+        }
+    }
+
+}
+
+class Picture {
+    private byte[] pixels;
+
+    public Picture(int length) {
+        this.pixels = new byte[length];
+    }
+}
